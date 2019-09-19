bc49d5f9207319cb5e1c4e5839644d12a8163132
Joe Klause <joeklause@gmail.com>
2019-09-18 22:10:50 -0700

    working functions and passed tests

--- __test__/document-collection.test.js
+++ __test__/document-collection.test.js
@@ -4,7 +4,7 @@ jest.mock('../lib/files.js', () => ({
   readdir: jest.fn(),
 }));
 
-const path = require('path')
+const path = require('path');
 
 const DocumentCollection = require('../lib/document-collection');
 
@@ -13,23 +13,71 @@ const { readFile, writeFile, readdir } = require('../lib/files');
 
 describe('Document Collection', () => {
 
-  it('saves file to path', () => {
-    //arrange
+
+  it('saves object to folder', () => {
     const folder = 'dest.txt';
     const docCollection = new DocumentCollection(folder);
     const object = {
       person: 'joe'
     };
-    writeFile.mockResolvedValue(object);
-    //act
+
+    writeFile.mockResolvedValueOnce(object);
+
     return docCollection.save(object)
       .then(obj => {
-      //assert
-        expect(obj.id).toEqual(expect.any(String));
+  
         const writeCalls = writeFile.mock.calls;
+
         expect(path.dirname(writeCalls[0][0])).toBe(folder);
         expect(writeCalls[0][1]).toBe(object);
+        expect(obj.id).toEqual(expect.any(String));
       });
   });
 
+
+  it('gets object from folder by id', () => {
+    const folder = 'dest.txt';
+    const docCollection = new DocumentCollection(folder);
+    const expectedObject = {
+      person: 'joe',
+      id: 'jlk'
+    };
+
+    readFile.mockResolvedValueOnce(JSON.stringify(expectedObject));
+
+    return docCollection.get(expectedObject.id)
+      .then(returnedObj => {
+
+        const readCalls = readFile.mock.calls;
+
+        expect(readCalls.length).toBe(1); 
+        expect(path.dirname(readCalls[0][0])).toBe(folder);
+        expect(returnedObj).toEqual(expectedObject);
+      });
+  });
+
+  
+  it('gets all objects from folder', () => {
+
+    const folder = 'dest.txt';
+    const docCollection = new DocumentCollection(folder);
+    const expectedObject = {
+      person: 'joe',
+      id: 'jlk'
+    };
+
+    readdir.mockResolvedValueOnce(['jlk.json']);
+    readFile.mockResolvedValueOnce(JSON.stringify(expectedObject));
+    
+    docCollection.getAll()
+      .then(returnArray => {
+
+        const readdirCalls = readdir.mock.calls;
+        const readCalls = readFile.mock.calls;
+
+        expect(readdirCalls[0][0]).toBe(folder);
+        expect(readCalls[0][0]).toBe(`${folder}/${expectedObject.id}.json`);
+        expect(returnArray[0].id).toBe(expectedObject.id);
+      });
+  });
 });
