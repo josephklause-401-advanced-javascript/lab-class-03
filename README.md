1c0fd163d042531399b8890f71a7430578e2efdb
Joe Klause <joeklause@gmail.com>
2019-09-18 17:58:07 -0700

    leaving scool

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
 # LAB - Class 03
 
-## Project Name
+## Async
 
-### Author: Student/Group Name
+### Author: Joseph Klause
 
 ### Links and Resources
 * [submission PR](http://xyz.com)
 * [travis](http://xyz.com)
-* [back-end](http://xyz.com) (when applicable)
-* [front-end](http://xyz.com) (when applicable)
+
 
 #### Documentation
-* [api docs](http://xyz.com) (API servers)
 * [jsdoc](http://xyz.com) (Server assignments)
-* [styleguide](http://xyz.com) (React assignments)
 
-### Setup
-#### `.env` requirements
-* `PORT` - Port Number
-* `MONGODB_URI` - URL to the running mongo instance/db
-
-**or, include an `.env.example`**
 
 #### Running the app
 
--- __test__/document-collection.test.js
+++ __test__/document-collection.test.js
@@ -1,12 +1,35 @@
-jest.mock('fs', () => ({
+jest.mock('../lib/files.js', () => ({
   readFile: jest.fn(),
   writeFile: jest.fn(),
   readdir: jest.fn(),
 }));
 
+const path = require('path')
+
+const DocumentCollection = require('../lib/document-collection');
+
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 
 describe('Document Collection', () => {
-  // TODO
+
+  it('saves file to path', () => {
+    //arrange
+    const folder = 'dest.txt';
+    const docCollection = new DocumentCollection(folder);
+    const object = {
+      person: 'joe'
+    };
+    writeFile.mockResolvedValue(object);
+    //act
+    return docCollection.save(object)
+      .then(obj => {
+      //assert
+        expect(obj.id).toEqual(expect.any(String));
+        const writeCalls = writeFile.mock.calls;
+        expect(path.dirname(writeCalls[0][0])).toBe(folder);
+        expect(writeCalls[0][1]).toBe(object);
+      });
+  });
+
 });
